e0947790d10f5fe932d727e048a3b25de82f1dba
함성준 <jun@hamseongjun-ui-Macmini.local>
2021-04-15 21:38:48 +0900

    Solution_1816

--- HamSungJun/String/Solution_1678.js
+++ HamSungJun/String/Solution_1678.js
@@ -2,20 +2,20 @@
  * @param {string} command
  * @return {string}
  */
- var interpret = function(command) {
-    const parseMap = new Map()
-    parseMap.set('G', 'G')
-    parseMap.set('(al)', 'al')
-    parseMap.set('()', 'o')
+const interpret = function(command) {
+  const parseMap = new Map();
+  parseMap.set('G', 'G');
+  parseMap.set('(al)', 'al');
+  parseMap.set('()', 'o');
 
-    let result = ''
-    let acc = ''
-    for(let i = 0; i < command.length; i++){
-        acc += command.charAt(i)
-        if(parseMap.has(acc)){
-            result += parseMap.get(acc)
-            acc = ''
-        }
+  let result = '';
+  let acc = '';
+  for (let i = 0; i < command.length; i++) {
+    acc += command.charAt(i);
+    if (parseMap.has(acc)) {
+      result += parseMap.get(acc);
+      acc = '';
     }
-    return result
+  }
+  return result;
 };

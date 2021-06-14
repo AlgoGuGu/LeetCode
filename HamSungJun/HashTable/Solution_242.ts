90568d5e44468b8ceb6f975bf186ee5bd19c59df
HamSungJun <tjdwns5123@gmail.com>
2021-06-14 21:36:46 +0900

    HamSungJun-242,690,706

---
+++ HamSungJun/HashTable/Solution_242.ts
@@ -0,0 +1,19 @@
+function isAnagram (s: string, t: string): boolean {
+  const countMap = new Map()
+  for (let i = 0; i < s.length; i++) {
+    const nextChar = s[i]
+    if (countMap.has(nextChar)) {
+      countMap.set(nextChar, countMap.get(nextChar) + 1)
+    } else {
+      countMap.set(nextChar, 1)
+    }
+  }
+  for (let i = 0; i < t.length; i++) {
+    const nextChar = t[i]
+    if (!countMap.has(nextChar) || countMap.get(nextChar) === 0) {
+      return false
+    }
+    countMap.set(nextChar, countMap.get(nextChar) - 1)
+  }
+  return true
+};
